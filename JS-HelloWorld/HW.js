8e06623145ad98efccc88af2c756fa4773687207
Abdellbaz <34577741+Abdellbaz@users.noreply.github.com>
2018-03-21 08:32:03 +0100

    Update HW.js

--- JS-HelloWorld/HW.js
+++ JS-HelloWorld/HW.js
@@ -1,6 +1,10 @@
+
+console.log('hallo world');
+console.log('Link: http://localhost:8080/');
+
 var http = require('http');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello World!');
+    res.end('hallo world!');
 }).listen(8080); 
