58ec708599fe25248f30eb75b9d7e70c356a7598
Abdellbaz <34577741+Abdellbaz@users.noreply.github.com>
2018-03-27 13:08:30 +0200

    Update GM.js

--- JS-Gemiddelde/GM.js
+++ JS-Gemiddelde/GM.js
@@ -1,11 +1,24 @@
-aantal =0;
-samen = 0;
-process.argv.splice(0, 2);
+let aantal =0;
+let samen = 0;
+process.argv.splice(0,2);
 
 for (let i in process.argv) {
-samen += Number(i);
+
 aantal++;
+samen += Number(process.argv[i]);
 }
-console.log("\naantal "+aantal);
-console.log("samen "+samen);
-console.log("gemiddelde "+samen/aantal);
+
+a="\naantal "+aantal;
+s="samen "+samen;
+g="gemiddelde "+samen/aantal;
+
+  console.log(a);
+  console.log(s);
+  console.log(g);
+
+var fs = require('fs');
+
+fs.writeFile('SaveGM.txt',a+"\n"+s+"\n"+g, function (err) {
+  if (err) throw err;
+  console.log('\nReplaced!');
+});
