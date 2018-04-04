5fb04732a6decabab43605b99862bab2ef148cfe
Abdellbaz <34577741+Abdellbaz@users.noreply.github.com>
2018-04-04 11:53:14 +0200

    Update from.php

--- form4_err/views/from.php
+++ form4_err/views/from.php
@@ -1,3 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="description" content="School">
+    <meta name="keywords" content="HTML,CSS,XML,JavaScript">
+    <meta name="author" content="Abdelaziz">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Abdelaziz Elbaze MG1D</title>
+</head>
+<body>
 <h1>Contactformulier</h1>
 <h3>Abdelaziz ELbaze GD1B</h3>
 <hr>
@@ -14,3 +25,5 @@
   </form>
 </pre>
 <hr>
+</body>
+</html>
