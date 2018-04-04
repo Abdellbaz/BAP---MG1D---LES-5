317daceac18b223e1d800be4f0d4d65f9209413b
Abdellbaz <34577741+Abdellbaz@users.noreply.github.com>
2018-04-04 11:52:45 +0200

    Update response.php

--- form4_err/views/response.php
+++ form4_err/views/response.php
@@ -1,6 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="description" content="School">
+    <meta name="keywords" content="HTML,CSS,XML,JavaScript">
+    <meta name="author" content="Abdelaziz">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Abdelaziz Elbaze MG1D</title>
+</head>
+<body>
 <?php
 echo '<h2>Hallo wereld! </h2>';
 echo 'Mijn naam is '.$_POST['name'].'<br/ >';
 echo 'Mijn e-mail is '.$_POST['e-mail'].'<br/ >';
 echo 'Ik zeg: '.$_POST['message'];
 ?>
+</body>
+</html>
