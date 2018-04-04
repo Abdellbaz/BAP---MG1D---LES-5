90f0f59f8389299a36f7624c94f818be3b63056d
Abdellbaz <34577741+Abdellbaz@users.noreply.github.com>
2018-04-04 11:50:42 +0200

    Update index.php

--- form4_err/index.php
+++ form4_err/index.php
@@ -1,10 +1,29 @@
-<?php 
-	// maak dit scipt werkend: zoek de fouten	
-	
-	include 'views/from.php';
-	
-	if (isset($_POST['sendContact']))
-	{
-		include 'views/response.php';
-	}
-?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="description" content="School">
+    <meta name="keywords" content="HTML,CSS,XML,JavaScript">
+    <meta name="author" content="Abdelaziz">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Abdelaziz Elbaze MG1D</title>
+</head>
+<body>
+<h1>Contactformulier</h1>
+<h3>Abdelaziz ELbaze GD1B</h3>
+<hr>
+<pre>
+  <form method="POST" action="index.php">
+
+    naam: 	 <input type="text" name="name" autofocus>
+
+    email: 	  <input type="email" name="e-mail">
+
+    bericht: 	<input type="text" name="message">
+
+    			    <input type="submit" name='sendContact'>
+  </form>
+</pre>
+<hr>
+</body>
+</html>
