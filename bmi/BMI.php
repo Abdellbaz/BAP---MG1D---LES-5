1969db62649f3dd63cf40472782fdd4376a26ca0
Abdellbaz <34577741+Abdellbaz@users.noreply.github.com>
2018-06-15 00:44:57 +0200

    Update BMI.php

--- bmi/BMI.php
+++ bmi/BMI.php
@@ -9,9 +9,10 @@
   $height=$height*$height;
   $bmi = round($weight/$height, 1);
   switch ($bmi) {
-    case $bmi >0   &&   $bmi <18.5:echo "Uw bmi is: ".$bmi."  Normaal/gezond";break;
-    case $bmi >=18.5 && $bmi <=24.9:echo "Uw bmi is: ".$bmi." Licht overgewicht";break;
-    case $bmi >=25 &&   $bmi <=26.9:echo "Uw bmi is: ".$bmi." Matig overgewicht";break;
+    case $bmi >0   &&   $bmi <18.5:echo "Uw bmi is: ".$bmi."  Ondergewicht";break;
+    case $bmi >=18.5 && $bmi <=24.9:echo "Uw bmi is: ".$bmi." Normaal/gezond";break;
+    case $bmi >=25 &&   $bmi <=26.9:echo "Uw bmi is: ".$bmi." Licht overgewicht";break;
+    case $bmi >=27 &&   $bmi <=29.9:echo "Uw bmi is: ".$bmi." Matig overgewicht";break;
     case $bmi >=30 &&   $bmi <=34.9:echo "Uw bmi is: ".$bmi." Obesitas(Class 1)";break;
     case $bmi >=35 &&   $bmi <=39.9:echo "Uw bmi is: ".$bmi." Obesitas(Class 2)";break;
     case $bmi >=40:echo "Uw bmi is: ".$bmi." Morbide obesitas";break;
